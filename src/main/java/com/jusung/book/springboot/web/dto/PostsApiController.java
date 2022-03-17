6d2ebee7f66df5e4a4fe3f057b3398f97f445079
kimjusung <dlrtls12345@naver.com>
2022-03-17 23:39:03 +0900

    jpa test2

---
+++ src/main/java/com/jusung/book/springboot/web/dto/PostsApiController.java
@@ -0,0 +1,28 @@
+package com.jusung.book.springboot.web.dto;
+
+import com.jusung.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        System.out.println("checked");
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
